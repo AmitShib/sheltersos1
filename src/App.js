2062b37974b0f921136faba79f9a9183b66d8297
Amit Shiber <amitshiber2001@gmail.com>
2024-04-25 18:04:13 +0300

    with signout button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState , useContext } from 'react';
 import './App.css';
 import MapComponent from './Map/MapComponent';
 import ShelterList from './UI/ShelterList';
@@ -6,7 +6,7 @@ import SignInButton from './UI/SignIn/SignInButton';
 import Modal from './UI/SignIn/Modal';
 import SignUpForm from './UI/SignIn/SignUpForm';
 import SignInForm from './UI/SignIn/SignInForm';
-import { GlobalProvider } from './GlobalContext';
+import { GlobalProvider , GlobalContext} from './GlobalContext';
 
 
 function App() {
@@ -16,6 +16,8 @@ function App() {
   const [showSignInModal, setShowSignInModal] = useState(false);
   const [showSignUpModal, setShowSignUpModal] = useState(false);
 
+  // const {setIsConnectedValue, setIsAdminValue } = useContext(GlobalContext);
+
   const handleSignInButtonClick = () => {
     setShowSignInModal(true);
   };
@@ -23,6 +25,11 @@ function App() {
   const handleSignUpButtonClick = () => {
     setShowSignUpModal(true);
   };
+
+//   const handleSignOutButtonClick = () => {
+//     setIsConnectedValue(false);
+//     setIsAdminValue(false);
+// };
 
   const handleCloseModal = () => {
     setShowSignInModal(false);
@@ -32,24 +39,34 @@ function App() {
 
   return (
     <GlobalProvider>
-      <div className="App">
-        <header className="App-header">
-          <MapComponent mapRef={mapRef} />
-          <ShelterList mapRef={mapRef} />
-          <SignInButton onSignInClick={handleSignInButtonClick} onSignUpClick={handleSignUpButtonClick} /> {/* Pass handlers for sign-in and sign-up buttons */}
-          {showSignInModal && (
-            <Modal onClose={handleCloseModal}>
-              <SignInForm onClose={handleCloseModal}/>
-            </Modal>
-          )}
-          {showSignUpModal && (
-            <Modal onClose={handleCloseModal}>
-              <SignUpForm onClose={handleCloseModal}/> {/* Pass onClose handler to close the modal */}
-            </Modal>
-          )}
-
-        </header>
-      </div>
+      <GlobalContext.Consumer>
+        {({ setIsConnectedValue, setIsAdminValue }) => (
+          <div className="App">
+            <header className="App-header">
+              <MapComponent mapRef={mapRef} />
+              <ShelterList mapRef={mapRef} />
+              <SignInButton
+                onSignInClick={handleSignInButtonClick}
+                onSignUpClick={handleSignUpButtonClick}
+                onSignOutClick={() => {
+                  setIsConnectedValue(false);
+                  setIsAdminValue(false);
+                }}
+              />
+              {showSignInModal && (
+                <Modal onClose={handleCloseModal}>
+                  <SignInForm onClose={handleCloseModal} />
+                </Modal>
+              )}
+              {showSignUpModal && (
+                <Modal onClose={handleCloseModal}>
+                  <SignUpForm onClose={handleCloseModal} />
+                </Modal>
+              )}
+            </header>
+          </div>
+        )}
+      </GlobalContext.Consumer>
     </GlobalProvider>
 
   );
--- src/UI/SignIn/SignInButton.js
+++ src/UI/SignIn/SignInButton.js
@@ -1,12 +1,28 @@
 // SignInButton.js
-import React from 'react';
+import React, { useContext } from 'react';
 import './SignInButton.css'; // Import the CSS file for styling
+import { GlobalContext } from '../../GlobalContext';
 
-const SignInButton = ({ onSignInClick, onSignUpClick }) => {
+const SignInButton = ({ onSignInClick, onSignUpClick, onSignOutClick }) => {
+
+  const { isConnected } = useContext(GlobalContext);
+
   return (
-    <div className='sign-button-container'>
-      <button className="sign-button" onClick={onSignInClick}>Sign In</button>
-      <button className="sign-button" onClick={onSignUpClick}>Sign Up</button>
+    <div className="sign-button-container">
+      {isConnected ? (
+        <button className="sign-button" onClick={onSignOutClick}>
+          Sign Out
+        </button>
+      ) : (
+        <>
+          <button className="sign-button" onClick={onSignInClick}>
+            Sign In
+          </button>
+          <button className="sign-button" onClick={onSignUpClick}>
+            Sign Up
+          </button>
+        </>
+      )}
     </div>
   );
 };
